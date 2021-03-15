93610ea3afa4bab4a41438144381028c37804f1f
Schneider Shades <schneidershades@gmail.com>
2021-03-15 19:55:16 +0100

    last loop

--- resources/views/livewire/file-browser.blade.php
+++ resources/views/livewire/file-browser.blade.php
@@ -19,10 +19,12 @@
                 <a href="{{route('files', ['uuid' => $ancestor->uuid])}}" class="font-bold text-gray-400">
                     {{ $ancestor->objectable->name }}
                 </a>
+                @if(!$loop->last)
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
                     class="text-gray-300 w-5 h-5 mx-1">
                     <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
                 </svg>
+                @endif
                 @endforeach
             </div>
         </div>
